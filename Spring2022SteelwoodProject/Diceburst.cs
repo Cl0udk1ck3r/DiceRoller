8cb8e2850c3530d887d854b873db1c2340eff2cf
Cl0udk1ck3r <96272542+Cl0udk1ck3r@users.noreply.github.com>
2022-10-22 01:27:09 -0400

    Add files via upload
    Dice Roller project

---
+++ Spring2022SteelwoodProject/Diceburst.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Spring2022SteelwoodProject
+{
+    class DiceBurst
+    {
+        //All Private Attributes
+        private Random RNG = new Random();
+        private int[] PlayerHand = new int[10];
+        private int HandSize = 0, playerscore = 0, opponentscore = 0;
+        private decimal funds = 1000, bet = 0;
+
+
+        //Getters
+        public int[] GetPlayerHand()
+        {
+            return PlayerHand;
+        }
+
+        public decimal GetFunds()
+        {
+            return funds;
+        }
+
+        public decimal GetBet()
+        {
+            return bet;
+        }
+
+        public int GetHandSize()
+        {
+            return HandSize;
+        }
+
+        public int GetPlayerScore()
+        {
+            return playerscore;
+        }
+
+        public int GetOpponentScore()
+        {
+            return opponentscore;
+        }
+
+        //Setters
+        public void SetFunds(decimal SetFunds)
+        {
+            funds = SetFunds;
+        }
+
+        public void SetBet(decimal SetBet)
+        {
+            bet = SetBet;
+        }
+
+        public void SetHandSize(int SetHandSize)
+        {
+            HandSize = SetHandSize;
+        }
+
+        public void SetPlayerHand(int[] SetPlayerHand)
+        {
+            PlayerHand = SetPlayerHand;
+        }
+
+        public void SetPlayerScore(int SetPlayerScore)
+        {
+            playerscore = SetPlayerScore;
+        }
+
+        public void SetOpponentScore(int SetOppScore)
+        {
+            opponentscore = SetOppScore;
+        }
+
+       
+        //Score Method
+        public void Score(int playerscore)
+        {
+            for (int i = 0; i < GetPlayerHand().Length; i++)
+            {
+                playerscore = PlayerHand[i] + playerscore;
+            }
+        }
+        //Roll Method
+        public void Roll(int index)
+        {
+            PlayerHand[index] = RNG.Next(1, 7);
+        }
+
+        //Reset Method
+        public void Reset(int[] HandArray)
+        {
+            for (int i = 0; i < HandArray.Length; i++)
+            {
+                HandArray[i] = 0;
+            }
+        }
+
+        //Play Method
+        public void Play(decimal bet, DiceBurst Opponent)
+        {
+            bool tie = false;
+
+            {
+                int count = 2;
+                //Dice roll for Players
+                Console.WriteLine("Rolling for you!");
+                for (int i = 0; i < GetPlayerHand().Length; i++)
+                {
+                    if (i < count)
+                        Roll(i);
+                    playerscore += PlayerHand[i];
+                    if (PlayerHand[i] != 0)
+                    {
+                        Console.WriteLine(PlayerHand[i]);
+                    }
+                    if (PlayerHand[i] == 6)
+                    {
+                        count++;
+                    }
+                }
+                SetPlayerScore(playerscore);
+                Console.WriteLine($"Your score is: {playerscore}");
+
+                Console.WriteLine("Rolling for your opponent!");
+                count = 2;
+                for (int i = 0; i < Opponent.GetPlayerHand().Length; i++)
+                {
+                    if (i < count)
+                        Opponent.Roll(i);
+                    Opponent.opponentscore += Opponent.PlayerHand[i];
+                    if (Opponent.PlayerHand[i] != 0)
+                    {
+                        Console.WriteLine(Opponent.PlayerHand[i]);
+                    }
+                    if (Opponent.PlayerHand[i] == 6)
+                    {
+                        count++;
+                    }
+                }
+                Opponent.SetOpponentScore(Opponent.opponentscore);
+                Console.WriteLine($"Your opponent's score is: {Opponent.opponentscore}");
+
+                //Money disbursement
+                if (GetPlayerScore() > Opponent.GetOpponentScore())
+                {
+                    SetFunds(GetFunds() + GetBet());
+                    Opponent.SetFunds(Opponent.GetFunds() - GetBet());
+                }
+
+                else if (Opponent.GetOpponentScore() > GetPlayerScore())
+                {
+                    SetFunds(GetFunds() - GetBet());
+                    Opponent.SetFunds(Opponent.GetFunds() + GetBet());
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Spring2022SteelwoodProject/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Threading;
+
+namespace Spring2022SteelwoodProject
+{
+    class Program
+
+    {
+
+
+        static void Main(string[] args)
+
+        { /*2022
+		   *Steelwood, Mars
+		   *Diceburst
+		   */
+
+            DiceBurst player = new DiceBurst();
+            DiceBurst player2 = new DiceBurst();
+            bool Continue = false;
+            decimal betvar = 0;
+
+            //Wincon Booleans
+  
+   
+
+
+            do //Matches (while funds <= 0)
+            {
+                do //Games (while funds > 0)
+                {
+                    
+                    bool flag2 = true;
+                    Console.WriteLine("Welcome to DiceBurst!");
+                    Thread.Sleep(1500);
+                    Console.WriteLine($"Your current funds: {player.GetFunds():c}. " +
+                        $"\nOpponent's current funds: {player2.GetFunds():c}");
+                    Thread.Sleep(850);
+                    Console.WriteLine("Get ready to play!");
+
+                    //Bet assignment
+
+                    Continue = false;
+                    
+                    
+                    do
+                    {
+                        Console.WriteLine("Place your bet now! \nWager wisely, and don't bet more than you have!");
+                        flag2 = decimal.TryParse(Console.ReadLine(), out betvar);
+                    } while (betvar > player.GetFunds() || betvar <= 0 || !flag2);
+                    player.SetBet(betvar);
+                    Console.WriteLine($"You bet {player.GetBet():C}. Let's play!");
+
+
+                    //Game Time
+                    
+                    player.Play(betvar, player2);
+                    
+                    bool playerwin = (player.GetPlayerScore() > player2.GetOpponentScore());
+                    bool playerloss = (player.GetPlayerScore() < player2.GetOpponentScore());
+
+                    //Game Wincons
+                    if (playerwin)
+                    {
+                        Console.WriteLine($"Current funds: \n You: {player.GetFunds():c} \n Opponent: {player2.GetFunds():c}");
+                        Console.WriteLine("You win! \n Press Y to play again, or anything else to quit.");
+                        char playagain = char.ToUpper(Console.ReadKey().KeyChar);
+                        if (playagain != 'Y')
+                        {
+                            Console.WriteLine("Thanks for playing!");
+                            Continue = false;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Get ready for a new game!");
+                            Continue = true;
+                            player.Reset(player.GetPlayerHand());
+                            player2.Reset(player2.GetPlayerHand());
+                            player.SetPlayerScore(0);
+                            player2.SetOpponentScore(0);
+                        }
+                    }
+                    else if (playerloss)
+                    {
+                        Console.WriteLine($"Current funds: \n You: {player.GetFunds():c} \n Opponent: {player2.GetFunds():c}");
+                        Console.WriteLine("You lose. \n Press Y to play again, or anything else to quit.");
+                        char playagain = char.ToUpper(Console.ReadKey().KeyChar);
+                        if (playagain != 'Y')
+                        {
+                            Console.WriteLine("Thanks for playing!");
+                            Continue = false;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Get ready for a new game!");
+                            Continue = true;
+                            player.Reset(player.GetPlayerHand());
+                            player2.Reset(player2.GetPlayerHand());
+                            player.SetPlayerScore(0);
+                            player2.SetOpponentScore(0);
+                        }
+                    }
+
+                    else if (player.GetPlayerScore() == player2.GetOpponentScore()) //Tie
+                    {
+                        Console.WriteLine("Tie! It happens sometimes. \n Press Y for a tiebreaker, or anything else to quit");
+                        char playagain = char.ToUpper(Console.ReadKey().KeyChar);
+                        if (playagain != 'Y')
+                        {
+                            Console.WriteLine("Thanks for playing!");
+                            Continue = false;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Time for tiebreaker!"); //No reset
+                            Continue = true;
+                        }
+                    }
+
+
+
+
+                } while (Continue == true && (player.GetFunds() > 0 && player2.GetFunds()>0));
+
+                //Match Wincons
+                Console.WriteLine($"Your funds: {player.GetFunds():c} \nOpponent funds: {player2.GetFunds():c}");
+                if (player.GetFunds() <=0)
+                {
+                    Console.WriteLine("Game over. You lose. \n If you'd like to play again press Y, or press anything else to quit");
+                    char playagain = char.ToUpper(Console.ReadKey().KeyChar);
+                    if (playagain != 'Y')
+                    {
+                        Console.WriteLine("Thanks for playing!");
+                        Continue = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Get ready for a new game!");
+                        player.SetFunds(1000);
+                        player2.SetFunds(1000);
+                        Continue = true;
+                        player.Reset(player.GetPlayerHand());
+                        player2.Reset(player2.GetPlayerHand());
+                        player.SetPlayerScore(0);
+                        player2.SetOpponentScore(0);
+                    }
+                }
+                else if (player2.GetFunds()<=0)
+                {
+                    Console.WriteLine("Game over. You win! \n If you'd like to play again press Y, or press anything else to quit.");
+                    char playagain = char.ToUpper(Console.ReadKey().KeyChar);
+                    if (playagain != 'Y')
+                    {
+                        Console.WriteLine("Thanks for playing!");
+                        Continue = false;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Get ready for a new game!");
+                        player.SetFunds(1000);
+                        player2.SetFunds(1000);
+                        Continue = true;
+                        player.Reset(player.GetPlayerHand());
+                        player2.Reset(player2.GetPlayerHand());
+                        player.SetPlayerScore(0);
+                        player2.SetOpponentScore(0);
+                    }
+                }
+
+
+
+            } while (Continue == true);
+
+
+        }
+    }
+}
+
+
